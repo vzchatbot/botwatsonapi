1ab0e2b0ea364177ac95506a2d590ed6bb268944
vzchatbot <vzchatbot@hotmail.com>
2016-11-01 19:46:15 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -57,9 +57,10 @@ function processEvent(event) {
                 var responseData = response.result.fulfillment.data;
                 var action = response.result.action;
 		console.log('responseText  : - '+ responseText);
-		console.log('responseData  : - '+ responseData);		    
- 		
-		     sendFBMessage(sender, responseText);
+		console.log('responseData  : - '+ responseData);
+		    
+ 		 sendFBMessage(sender, responseText);
+		    
                 if (isDefined(responseText) || isDefined(responseText.facebook)) {
 			console.log('first  : - ');
                     if (!Array.isArray(responseText)) {
@@ -70,8 +71,25 @@ function processEvent(event) {
                         } catch (err) {
                             sendFBMessage(sender, {text: err.message});
                         }
-                    } 
-			else if (isDefined(responseData)) {
+                    } else {
+                        responseText.facebook.forEach(function (facebookMessage)  {
+                            try {
+                                if (facebookMessage.sender_action) {
+                                    console.log('Response as sender action');
+                                    console.log("facebookMessage.sender_action" + facebookMessage.sender_action);
+                                    sendFBSenderAction(sender, facebookMessage.sender_action);
+                                }
+                                else {
+                                    console.log('Response as formatted message');
+                                    console.log("facebookMessage"+facebookMessage);
+                                    sendFBMessage(sender, facebookMessage);
+                                }
+                            } catch (err) {
+                                sendFBMessage(sender, {text: err.message});
+                            }
+                        });
+                    }
+                } else if (isDefined(responseData)) {
                     console.log('Response as data message'+ responseText);
                     // facebook API limit for text length is 320,
                     // so we must split message if needed
@@ -89,6 +107,7 @@ function processEvent(event) {
         apiaiRequest.end();
     }
 }
+
 
 function splitResponse(str) {
     if (str.length <= 320) {
