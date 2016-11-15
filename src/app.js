18c1f5d402195ddaa702e5c69c6c3c09f468980f
vzchatbot <vzchatbot@hotmail.com>
2016-11-15 16:58:52 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -46,7 +46,22 @@ function processEvent(event) {
 
     if ((event.message && event.message.text) || (event.postback && event.postback.payload)) {
         var text = event.message ? event.message.text : event.postback.payload;      
-	    
+	  console.log("Before Account Linking ");  
+	//account linking check
+        if (session.message.sourceEvent.account_linking == undefined) 
+	{
+            console.log("Account Linking null");
+        }
+        else {
+		 console.log("inside Account Linking ");  
+            console.log("Account Linking convert: " + JSON.stringify(session.message.sourceEvent.account_linking, null, 2));
+            console.log("Account Linking convert: " + JSON.stringify(session.message.sourceEvent.account_linking.authorization_code, null, 2));
+            console.log("Account Linking convert: " + JSON.stringify(session.message.sourceEvent.account_linking.status, null, 2));
+  	    session.send("Your account is linked now.");
+		getVzProfile(session,function (str){ getVzProfileCallBack(str,session)});   
+		MainMenu(session);
+	
+        }    
      
         var apiaiRequest  = apiAiService.textRequest(text,{sessionId: sessionIds.get(sender)});
         apiaiRequest .on('response', function (response)  {
