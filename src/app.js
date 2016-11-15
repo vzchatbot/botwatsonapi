fe6139cf3d345df54fcd13ee54f75f8793f41727
vzchatbot <vzchatbot@hotmail.com>
2016-11-15 12:09:26 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -193,7 +193,7 @@ function chunkString(s, len) {
 
 function sendFBMessage(sender, messageData, callback) {
 	
-	   console.log('sendFBMessage: sender '+ sender + "messageData  " + messageData);
+	 //  console.log('sendFBMessage: sender '+ sender + "messageData  " + messageData);
     request({
         url: 'https://graph.facebook.com/v2.6/me/messages',
         qs: {access_token: FB_PAGE_ACCESS_TOKEN},
