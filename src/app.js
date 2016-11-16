61b770f35fd4165c6485e3d5c021d8ea7b583514
vzchatbot <vzchatbot@hotmail.com>
2016-11-16 18:35:21 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1223,7 +1223,7 @@ function stationsearch(usersession)
 	
 	var respobj ={"facebook":{"text":"You can watch it at ","channel":["#-899- HBO HD","#-400- HBO","#-902- HBO 2 HD","#-402- HBO 2","#-903- HBO 2 West HD","#-403- HBO 2 West","#-908- HBO Comedy HD","#-408- HBO Comedy","#-909- HBO Comedy West HD","#-409- HBO Comedy West","#-906- HBO Family HD","#-406- HBO Family","#-907- HBO Family West HD","#-407- HBO Family West","#-912- HBO Latino HD","#-412- HBO Latino","#-913- HBO Latino West HD","#-413- HBO Latino West","#-904- HBO Signature HD","#-404- HBO Signature","#-905- HBO Signature West HD","#-405- HBO Signature West","#-901- HBO West HD","#-401- HBO West","#-910- HBO Zone HD","#-410- HBO Zone","#-911- HBO Zone West HD","#-411- HBO Zone West"]}};
 	 if (respobj.facebook) {
-            let entries = respobj.facebook.channel;
+            let entries = respobj.facebook;
 		 console.log("entries: "+entries);
             entries.forEach((channel) => {
                sendFBMessage(usersession,  channel);}
