fd018ec9a3af43627e41b1b27b05337608a9474e
vzchatbot <vzchatbot@hotmail.com>
2016-11-16 16:47:23 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1215,7 +1215,7 @@ function demowhatshot(usersession)
 function stationsearch(usersession) 
 {
     var respobj =  {"facebook":{"text":"You can watch it at           Position: 400   Channel: HBO           Position: 401   Channel: HBO West           Position: 402   Channel: HBO 2           Position: 403   Channel: HBO 2 West           Position: 404   Channel: HBO Signature           Position: 405   Channel: HBO Signature West           Position: 406   Channel: HBO Family           Position: 407   Channel: HBO Family West           Position: 408   Channel: HBO Comedy           Position: 409   Channel: HBO Comedy West           Position: 410   Channel: HBO Zone           Position: 411   Channel: HBO Zone West           Position: 412   Channel: HBO Latino           Position: 413   Channel: HBO Latino West           Position: 899   Channel: HBO HD "}};
-   sendFBMessage(usersession,  respobj.facebook);
+	  sendFBMessage(usersession,  respobj.facebook);
 }
 function testmethod(usersession)
 {
