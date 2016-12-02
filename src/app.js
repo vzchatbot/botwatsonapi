0a6ba897396d65a039dd0f2b4488f2aa71c1b548
vzchatbot <vzchatbot@hotmail.com>
2016-12-02 15:59:00 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -85,13 +85,13 @@ function processEvent(event) {
             console.log("Inside sessionID:- ");
             sessionIds.set(sender, uuid.v1());
         }
-	
+	/*
 	var ReqSenderID = event.sender.id.toString();
         var ReqRecipientID = event.recipient.id.toString();
         var ReqTimeStamp = event.timestamp.toString();        
         var ReqMessageID = event.message.mid.toString();
         var ReqMessageText = event.message.text.toString();   
-	
+	*/
 	    
         console.log("Text Value", text);   
         console.log("event content :- " +JSON.stringify(event.entry));
